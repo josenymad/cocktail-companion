a383124096decfc432fc1e7aeabdfe8d9fef3ccf
Jose Wills <jose.wills3@gmail.com>
2023-07-18 21:04:21 +0100

    added navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
 
 const NavBar = () => {
   return (
@@ -7,6 +9,23 @@ const NavBar = () => {
         <h1 className="navbar-title__header">Cocktail Companion</h1>
         <h2 className="navbar-title__slogan">The Ultimate Cocktail Guide</h2>
       </div>
+      <ul className="navbar-list">
+        <li className="navbar-list__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navbar-list__item">
+          <Link to="best-barware">Best Barware</Link>
+        </li>
+        <li className="navbar-list__item">
+          <Link to="all-cocktails">All Cocktails</Link>
+        </li>
+        <li className="navbar-list__item">
+          <form>
+            <label htmlFor="search">{}</label>
+            <input type="search" id="search" />
+          </form>
+        </li>
+      </ul>
     </div>
   );
 };
