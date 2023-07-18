c6de08022da8d94c0fdbf60686ec5e26f994ec47
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-07-18 21:29:20 +0100

    Add routes to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "../styles/App.css";
 import NavBar from "./NavBar";
 
@@ -6,6 +7,11 @@ const App = () => {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="best-barware" element={<BestBarware />} />
+        <Route path="all-cocktails" element={<AllCocktails />} />
+      </Routes>
     </div>
   );
 };
