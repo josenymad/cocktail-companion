b250ae40ab0f881a2ac0d550960231b62b21e4a3
Jose Wills <jose.wills3@gmail.com>
2023-07-30 12:57:02 +0100

    changed class names between burger nav and normal nav to differentiate styling between the two

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ import "../styles/title.css";
 
 const Title = () => {
   return (
-    <div className="navbar">
+    <div className="title">
       <div className="navbar-titles" id="outer-container">
         <div id="page-wrap">
           <h1 className="navbar-title__header">Cocktail Companion</h1>
