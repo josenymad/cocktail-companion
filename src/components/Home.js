b0f144512049f2ea5b6ed267240d1ec6e77954e3
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-01 08:46:13 +0100

    Replace API images with cocktail cards in carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import "../styles/home.css";
 import Carousel from "./Carousel";
 
-const Home = ({ drinksData }) => {
+const Home = ({ drinksData, selectedDrink, setSelectedDrink }) => {
   const { drinks } = drinksData;
 
   return (
     <div className="home">
-      <Carousel slides={drinks} />
+      <Carousel
+        selectedDrink={selectedDrink}
+        setSelectedDrink={setSelectedDrink}
+        slides={drinks}
+      />
       <p className="home__text">
         Welcome to Cocktail Companion! We have got you covered with an extensive
         range of delicious drinks, barware to suit all budgets and easy to
