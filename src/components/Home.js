3fab2bf46e400df1f21540dce5f8fe1f30d664d3
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-01 13:58:53 +0100

    Add and style landing page instructions

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,18 +7,32 @@ const Home = ({ drinksData, selectedDrink, setSelectedDrink }) => {
 
   return (
     <div className="home">
-      <Carousel
-        selectedDrink={selectedDrink}
-        setSelectedDrink={setSelectedDrink}
-        slides={drinks}
-      />
-      <p className="home__text">
-        Welcome to Cocktail Companion! We have got you covered with an extensive
-        range of delicious drinks, barware to suit all budgets and easy to
-        follow instructions to get you mixing cocktails in no time. Search by
-        ingredient, check out our most popular drinks or let us surprise you
-        with a random cocktail selection!
-      </p>
+      <div className="home__main">
+        <Carousel
+          selectedDrink={selectedDrink}
+          setSelectedDrink={setSelectedDrink}
+          slides={drinks}
+        />
+        <p className="home__text">
+          Welcome to Cocktail Companion! We have got you covered with an
+          extensive range of delicious drinks, barware to suit all budgets and
+          easy to follow instructions to get you mixing cocktails in no time.
+        </p>
+      </div>
+      <div className="home__guide">
+        <p className="home__guide-text">
+          Need inspiration? Scroll through our cocktail selection above and
+          click on any drink for ingredients and instructions.
+        </p>
+        <p className="home__guide-text">
+          Looking to perfect your set up? Head to Best Barware for ingredients
+          and equipment tips to suit a range of budgets.
+        </p>
+        <p className="home__guide-text">
+          After something specific? Check out All Cocktails where you can search
+          by name, filter by spirit and view our collections.
+        </p>
+      </div>
     </div>
   );
 };
