a44113701cad93dadc5bc623d86546f7cde482ea
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-02 12:37:22 +0100

    Reorganiselanding page layout and update media queries

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,25 +13,28 @@ const Home = ({ drinksData, selectedDrink, setSelectedDrink }) => {
           setSelectedDrink={setSelectedDrink}
           slides={drinks}
         />
-        <p className="home__text">
-          Welcome to Cocktail Companion! We have got you covered with an
-          extensive range of delicious drinks, barware to suit all budgets and
-          easy to follow instructions to get you mixing cocktails in no time.
-        </p>
-      </div>
-      <div className="home__guide">
-        <p className="home__guide-text">
-          Need inspiration? Scroll through our cocktail selection above and
-          click on any drink for ingredients and instructions.
-        </p>
-        <p className="home__guide-text">
-          Looking to perfect your set up? Head to Best Barware for ingredients
-          and equipment tips to suit a range of budgets.
-        </p>
-        <p className="home__guide-text">
-          After something specific? Check out All Cocktails where you can search
-          by name, filter by spirit and view our collections.
-        </p>
+        <div className="text">
+          <p className="text__intro">
+            Welcome to Cocktail Companion! We have got you covered with an
+            extensive range of delicious drinks, top tips for building your home
+            bar and easy to follow instructions to get you mixing cocktails in
+            no time.
+          </p>
+          <div className="guide">
+            <p className="guide__text">
+              Need inspiration? Scroll through our cocktail selection and click
+              on any drink for ingredients and instructions.
+            </p>
+            <p className="guide__text">
+              Looking to perfect your set up? Head to Best Barware for
+              ingredients and equipment tips to suit a range of budgets.
+            </p>
+            <p className="guide__text">
+              After something specific? Check out All Cocktails to search by
+              name, filter by spirit and view our collections.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
