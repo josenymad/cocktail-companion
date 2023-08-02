1a2f3492f150b74f0998430eb1c0f8a509b2bf1b
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-02 15:20:08 +0100

    Redo landing page layout with grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,33 +8,31 @@ const Home = ({ drinksData, selectedDrink, setSelectedDrink }) => {
   return (
     <div className="home">
       <div className="home__main">
-        <Carousel
-          selectedDrink={selectedDrink}
-          setSelectedDrink={setSelectedDrink}
-          slides={drinks}
-        />
-        <div className="text">
-          <p className="text__intro">
-            Welcome to Cocktail Companion! We have got you covered with an
-            extensive range of delicious drinks, top tips for building your home
-            bar and easy to follow instructions to get you mixing cocktails in
-            no time.
-          </p>
-          <div className="guide">
-            <p className="guide__text">
-              Need inspiration? Scroll through our cocktail selection and click
-              on any drink for ingredients and instructions.
-            </p>
-            <p className="guide__text">
-              Looking to perfect your set up? Head to Best Barware for
-              ingredients and equipment tips to suit a range of budgets.
-            </p>
-            <p className="guide__text">
-              After something specific? Check out All Cocktails to search by
-              name, filter by spirit and view our collections.
-            </p>
-          </div>
+        <div className="home__carousel">
+          <Carousel
+            selectedDrink={selectedDrink}
+            setSelectedDrink={setSelectedDrink}
+            slides={drinks}
+          />
         </div>
+        <p className="home__intro">
+          Welcome to Cocktail Companion! We have got you covered with an
+          extensive range of delicious drinks, top tips for building your home
+          bar and easy to follow instructions to get you mixing cocktails in no
+          time.
+        </p>
+        <p className="home__guide" id="guide-one">
+          Need inspiration? Scroll through our cocktail selection and click on
+          any drink for ingredients and instructions.
+        </p>
+        <p className="home__guide" id="guide-two">
+          Looking to perfect your set up? Head to Best Barware for ingredients
+          and equipment to suit a range of budgets.
+        </p>
+        <p className="home__guide" id="guide-three">
+          After something specific? Check out All Cocktails to search by name,
+          filter by spirit and view our collections.
+        </p>
       </div>
     </div>
   );
